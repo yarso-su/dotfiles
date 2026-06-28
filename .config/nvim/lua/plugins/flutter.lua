e2e3e71e14c011d2fc3ad14b3ad983fccda93fbd
Yeremi Ríos <yarso@proton.me>
2026-06-28 09:24:24 -0600

    fix(.config/nvim): remove flutter tools deprecated option

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -1,22 +1,22 @@
 return {
-{
-  'akinsho/flutter-tools.nvim',
-  lazy = false,
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'stevearc/dressing.nvim',
-  },
-  config = function()
-    require('flutter-tools').setup {
-      lsp = {
-        color = {
-          enabled = true,
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = function()
+      require("flutter-tools").setup {
+        lsp = {
+          on_attach = function(client, bufnr)
+            if client.supports_method "textDocument/documentColor" then
+              vim.lsp.document_color.enable(true, { bufnr = bufnr })
+            end
+          end,
+          capabilities = require("cmp_nvim_lsp").default_capabilities(),
         },
-        on_attach = function(client, bufnr)
-        end,
-        capabilities = require('cmp_nvim_lsp').default_capabilities(),
-      },
-    }
-  end
-}
+      }
+    end,
+  },
 }
